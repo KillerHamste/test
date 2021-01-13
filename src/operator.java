7852462083ae8ec079cf8c6deecfeb67c8cf3730
Killer <dinis-dikii@mail.ru>
2021-01-13 19:09:50 +0300

    доделал

--- src/operator.java
+++ src/operator.java
@@ -1,4 +1,4 @@
-import static java.lang.Math.round;
+import static java.lang.Math.*;
 
 public class operator {
 
@@ -14,19 +14,7 @@ public class operator {
 
         return bool;
     }
-/*
-    public boolean checkOperator(String input) {
-        boolean bool;
-        if (!input.equals("*") || !input.equals("-") ||
-                !input.equals("/") || !input.equals("+")) {
-            bool = false;
-        } else {
-            bool = true;
-        }
 
-//        return bool;
-//    }
-//*/
     public int doOperate(int num1, int num2, String operator) {
         int result = 0;
 
@@ -36,7 +24,7 @@ public class operator {
                 return result;
 
             case "/":
-                result = round(num1 / num2);
+                result = (int)floor(num1 / num2)-1;
                 return result;
 
             case "+":
